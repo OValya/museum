37bdd58ebe0de30bd8ee1cf0111f5212acda4087
OValya <valya.eto.ya@gmail.com>
2024-05-28 16:48:10 +0300

    feat: add mapbox library and layout section Contacts

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './style.css'
 import container from "./gallery.js";
 
+
+
 //--------WELCOME
 const slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
@@ -154,4 +156,15 @@ sliderRange.addEventListener('input', (e)=>{
     sliderLine.style.left = value;
     }
 )
+
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidmFseWExOTg2IiwiYSI6ImNsd3FldW9scTAxMHUyaXM5ZGxvMnFlM3IifQ.m2fJStbAbwQPn-fyMLsW4w';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/valya1986/cku5puds62tr418o7lnkicmip',
+    center: [2.336368, 48.861019],
+    zoom: 16
+});
+
 
