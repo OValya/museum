e264a1d74f37f237afdab716cef208ff48283dd2
unknown <valya.eto.ya@gmail.com>
2024-06-06 16:15:58 +0300

    feat: add video player(controls dont work...)

--- main.js
+++ main.js
@@ -134,11 +134,23 @@ volumeRange.addEventListener('input', e => {
 
 
 const iframeContainer = document.querySelector('.video-slide__container');
-function createFrame(srcDoc, src){
+const iframePlayer = document.querySelector('.video-player');
+function createFrame( link, img, width='452', height='254',){
     const frame = document.createElement('iframe');
-    frame.srcdoc = srcDoc;
-    frame.width='452'
-    frame.height='254';
+    frame.srcdoc = `<style>
+                            *{overflow: hidden;padding:0;margin:0;}
+                            html,body{height:100%; transition: transform .5s;}
+                            span, img{position:absolute;width:100%;top:0;bottom:0;margin:auto; transition: transform .5s;}
+                            span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}
+                            html:hover span{color: darkgoldenrod}
+                            html:hover img{transform: scale(1.1); transition: transform .5s;}
+                            </style>
+                            <a href= ${link}>
+                            <img src=${img} alt='Video youtube' width='452'>
+                            <span>▶</span>
+                        </a>`;
+    frame.width=width;
+    frame.height=height;
     frame.loading='lazy';
     frame.allowFullscreen=true;
     frame.allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
@@ -146,57 +158,45 @@ function createFrame(srcDoc, src){
     return frame;
 }
 const iframeData = [
-    {
-        img: './assets/video/poster0.jpg',
-        link: "https://www.youtube.com/embed/Vi5D6FKhRmo?autoplay=1&mute=1&si=ttxrEYDeR6xOHy-j&enablejsapi=1"
-    },
+
     {
         img: './assets/video/poster1.jpg',
-        link:  'https://www.youtube.com/embed/zp1BXPX8jcU?autoplay=1&mute=1&si=cCIlmtrksx1PTrtn&enablejsapi=1'
+        link:  'https://www.youtube.com/embed/zp1BXPX8jcU?controls=0&autoplay=1&mute=1&si=cCIlmtrksx1PTrtn&enablejsapi=1'
     },
     {
         img: './assets/video/poster2.jpg',
-        link: 'https://www.youtube.com/embed/NOhDysLnTvY?autoplay=1&mute=1&si=W_xV3pTHPFWetomC&enablejsapi=1'
+        link: 'https://www.youtube.com/embed/NOhDysLnTvY?controls=0&autoplay=1&mute=1&si=W_xV3pTHPFWetomC&enablejsapi=1'
     },
     {
         img: './assets/video/poster3.jpg',
-        link: 'https://www.youtube.com/embed/aWmJ5DgyWPI?autoplay=1&mute=1&si=SADA7qh4w4Z7u70G&enablejsapi=1'
+        link: 'https://www.youtube.com/embed/aWmJ5DgyWPI?controls=0&autoplay=1&mute=1&si=SADA7qh4w4Z7u70G&enablejsapi=1'
     },
     {
         img: './assets/video/poster4.jpg',
         link: 'https://www.youtube.com/embed/2OR0OCr6uRE?autoplay=1&mute=1&si=KnCyZFHZP4UP9NIg&enablejsapi=1'
+    },
+    {
+        img: './assets/video/poster0.jpg',
+        link: "https://www.youtube.com/embed/Vi5D6FKhRmo?autoplay=1&mute=1&si=ttxrEYDeR6xOHy-j&enablejsapi=1"
     }
 ]
-const srcDocs = [];
-iframeData.map(data =>{
-    const srcDoc = `<style>
-                            *{overflow: hidden;padding:0;margin:0;}
-                            html,body{height:100%; transition: transform .5s;}
-                            span, img{position:absolute;width:100%;top:0;bottom:0;margin:auto}
-                            span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}
-                            html:hover span{color: darkgoldenrod}
-                            html:hover img{transform: scale(1.1); transition: transform .5s;}
-                            </style>
-                            <a href= ${data.link}>
-                            <img src=${data.img} alt='Video youtube' width='452'>
-                            <span>▶</span>
-                        </a>`;
-    srcDocs.push(srcDoc)
-}
-    
-)
-for (let i = 0; i < srcDocs.length; i++) {
-    const iframe = createFrame(srcDocs[i], iframeData[i].link);
-    iframeContainer.append(iframe)
-}
+
+
+iframeData.forEach(it => {
+
+    iframeContainer.append(createFrame(it.link, it.img));
+    iframePlayer.append(createFrame(it.link, it.img, '1440px', '650px'))
+
+})
+
+
 
 const frames = window.frames;
 
 for (let i = 0; i < frames.length; i++) {
     frames[i].postMessage( '{"event":"command", "func":"pauseVideo", "args":""}', '*')
 }
-// frames.(fr => fr.contentWindow.postMessage( '{"event":"command", "func":"pauseVideo", "args":""}', '*'))
-// console.log(frames)
+
 
 //explore
 const sliderRange = document.querySelector(".slider-range")
